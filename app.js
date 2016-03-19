9ea59bb138a3a7576883358187bfe849702a2c2a
JoseRoman <jose_roman32@hotmail.com>
2016-03-19 16:11:16 -0400

    endpoints

--- app.js
+++ app.js
@@ -4,6 +4,24 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'jose32',
+  database : 'gameday'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+
+//connection.connect();
 
 var routes = require('./routes/index');
 var players = require('./routes/players');
@@ -22,13 +40,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', function(req, res){
-  res.render('index');
+app.use('/:team/players', function(req, res){
+
+  var team = req.params.team;
+  var query = "SELECT * FROM playersummary WHERE team='" + team + "'";
+
+  connection.query(query, function(err, rows, fields) {
+    if (err) throw err;
+
+    res.json(rows);
+
+  });
+
 });
 
-app.use('/:team/players', function(req, res){
-  
+
+app.use("/metrics", function(req, res){
+
+  var playerId = req.body.playerId;
+  var year = req.body.year;
+  var selectedMetric = req.body.selectedMetric;
+
+  //SELECT `GoodnessMetric1`, `Count`, `PitchType` FROM `dummyexampletable` WHERE `Year`=2015 AND `BatterID`=112526
+  var query = "SELECT * FROM playersummary WHERE team='" + team + "' LIMIT 5";
+
+  connection.query(query, function(err, rows, fields) {
+    if (err) throw err;
+
+    res.json(rows);
+
+  });
+
 });
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
